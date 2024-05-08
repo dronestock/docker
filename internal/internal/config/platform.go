ed3f52f3bc1e0bb501cf35149febc453bf540fd5
storezhang <storezhang@gmail.com>
2024-05-08 10:33:09 +0800

    feat(项目): 完善多平台编译支持

--- internal/internal/config/platform.go
+++ internal/internal/config/platform.go
@@ -11,15 +11,21 @@ type Platform struct {
 	// 操作系统
 	Os string `default:"${OS=linux}" json:"os,omitempty" validate:"omitempty,oneof=linux windows"`
 	// 架构
-	Arch string `default:"${ARCH}" json:"arch,omitempty" validate:"omitempty,oneof=amd64 i386 arm/v7 arm64"`
+	Arch string `default:"${ARCH}" json:"arch,omitempty" validate:"omitempty,oneof=amd64 i386 arm arm64"`
+	// 变体
+	Variant string `default:"${VARIANT}" json:"variant,omitempty" validate:"required_if=Arch arm,oneof=v5 v6 v7"`
 }
 
-func (p *Platform) Argument() (platform string) {
+func (p *Platform) Argument() string {
+	builder := gox.StringBuilder()
 	if "" != p.Os && "" != p.Arch {
-		platform = gox.StringBuilder(p.Os, constant.Slash, p.Arch).String()
+		builder.Append(p.Os).Append(constant.Slash).Append(p.Arch)
+	}
+	if "" != p.Os && "" != p.Arch && "" != p.Variant {
+		builder.Append(p.Variant)
 	}
 
-	return
+	return builder.String()
 }
 
 func (p *Platform) Qemu() bool {
