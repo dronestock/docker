bedefc7c99568e73ca19663e3d723440be95e871
storezhang <storezhang@gmail.com>
2024-05-09 20:15:24 +0800

    fix(推送): 修复协程WaitGroup值设置大于实际使用协程数量的问题
    [Skip CI]

--- internal/internal/config/binary.go
+++ internal/internal/config/binary.go
@@ -2,7 +2,7 @@ package config
 
 type Binary struct {
 	// 执行程序
-	Docker string `default:"${EXE=/usr/bin/docker}" json:"docker,omitempty"`
+	Docker string `default:"${DOCKER=/usr/bin/docker}" json:"docker,omitempty"`
 	// 执行程序
 	Daemon string `default:"${DAEMON=/usr/bin/dockerd}" json:"daemon,omitempty"`
 }
